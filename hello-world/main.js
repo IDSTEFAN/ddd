d1e9032e05a3f46d923c0158992431ab0295524e
IDSTEFAN <54887368+IDSTEFAN@users.noreply.github.com>
2019-09-28 20:17:46 +0800

    Add files via upload

---
+++ hello-world/main.js
@@ -0,0 +1,9 @@
+var app=new Vue({
+    el:'#app',
+    data:{
+        name: null,
+        age:null,
+        sex:null,
+        foodList:['葱','姜','孀']
+    }
+})
---
+++ hello-world/v-for.js
@@ -0,0 +1,10 @@
+var app=new Vue({
+    el:'#app',
+    data:{
+        foodList:[
+            {name:'可乐',price:3,discount:0.9},
+            {name:'薯条',price:5,discount:0.8},
+            {name:'炸鸡',price:10,discount:0},
+        ]
+    }
+})
